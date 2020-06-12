3bca7d713900d198b1ca9606e916b5302c7ff28a
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-12 15:19:04 -0500

    added for loop for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,87 @@
 var startQuizBtnEl = document.querySelector("#startquiz");
-var questionEl = document.querySelector(".title");
-var questionsContainerEl = document.querySelector(".questions-container");
+var titleEl = document.querySelector(".title");
+var startPageEl = document.querySelector(".start-page");
+var questionPageEl = document.querySelector(".question-page");
+var endPageEl = document.querySelector(".end-page");
 var pageContentEl = document.querySelector("main");
+var buttonContainerEl = document.querySelector(".button-container");
+
 
 var startQuiz = function() {
-    console.log(questionsContainerEl);
-    // change title to question
-    questionEl.textContent = questionsArray[0].question;
+    debugger;
+    // hide start page
+    pageContentEl.removeChild(startPageEl);
 
-    // remove start quiz button and quiz instructions
-    questionsContainerEl.removeChild(startQuizBtnEl);
-    questionsContainerEl.removeChild(document.querySelector("p"));
+    // loop through questions 
+   for (i = 0; i < questionsArray.length; i++) {
+        
+        // add question to page
+        var question = document.createElement("h1");
+        question.className = "question";
+        question.textContent = questionsArray[i].q;
+        questionPageEl.insertBefore(question, buttonContainerEl);
 
-    pageContentEl.className = "question-content";
+        // add all four buttons
+        buttonContainerEl.innerHTML = "<button class='btn question-btn'>" + questionsArray[i].a1 + "</button><button class=' btn question-btn'>" + questionsArray[i].a2 + "</ button><button class='btn question-btn'>" + questionsArray[i].a3 + "</button><button class='btn question-btn'>" + questionsArray[i].a4 + "</button>";
 
-    // add all four buttons
-    questionsContainerEl.innerHTML = "<button class='btn question-btn'>" + questionsArray[0].answerOne + "</button><button class=' btn question-btn'>" + questionsArray[0].answerTwo + "</ button><button class='btn question-btn'>" + questionsArray[0].answerThree + "</button><button class='btn question-btn'>" + questionsArray[4].answerFour + "</button>";
+        // how do I wait for question to be answered to move on to the next question???
+
+        };
 };
+
+var questionAnswered = function (event) {
+    var answer = event.target;
+     
+    if (answer.textContent === questionsArray[0].correct) {
+        var correctAnswer = document.createElement("p");
+        correctAnswer.className = "correct";
+        correctAnswer.textContent= "Correct!";
+        questionPageEl.appendChild(correctAnswer);
+    }
+}
 
 var questionsArray = [
     {
-        question: "Commonly used data types DO NOT include:",
-        answerOne: "1. strings",
-        answerTwo: "2. boolean",
-        answerThree: "3. alerts",
-        answerFour: "4. numbers",
+        q: "Commonly used data types DO NOT include:",
+        a1: "1. strings",
+        a2: "2. boolean",
+        a3: "3. alerts",
+        a4: "4. numbers",
+        correct: "3. alerts"
     } , 
     {
-        question: "The condition in an if/else statement is enclosed with ____________.",
-        answerOne: "1. quotes",
-        answerTwo: "2. curly brackets",
-        answerThree: "3. parentheses",
-        answerFour: "4. square brackets"
+        q: "The condition in an if/else statement is enclosed with ____________.",
+        a1: "1. quotes",
+        a2: "2. curly brackets",
+        a3: "3. parentheses",
+        a4: "4. square brackets", 
+        correct: "a3"
     } , 
     {
-        question: "Arrays in Javascript can be used to store: ",
-        answerOne: "1. number and strings",
-        answerTwo: "2. other arrays", 
-        answerThree: "3. booleans",
-        answerFour: "4. all of the above"
+        q: "Arrays in Javascript can be used to store: ",
+        a1: "1. number and strings",
+        a2: "2. other arrays", 
+        a3: "3. booleans",
+        a4: "4. all of the above",
+        correct: "a3"
     } , 
     {
-        question: "String values must be enclosed within __________ when being assigned to variables.",
-        answerOne: "1. commas",
-        answerTwo: "2. curly brackets", 
-        answerThree: "3. quotes",
-        answerFour: "4. parentheses"
+        q: "String values must be enclosed within __________ when being assigned to variables.",
+        a1: "1. commas",
+        a2: "2. curly brackets", 
+        a3: "3. quotes",
+        a4: "4. parentheses",
+        correct: "a3"
     } , 
     {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-        answerOne: "1. Javascript",
-        answerTwo: "2. Terminal Bash",
-        answerThree: "3. for loops",
-        answerFour: "4. console.log"
+        q: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        a1: "1. Javascript",
+        a2: "2. Terminal Bash",
+        a3: "3. for loops",
+        a4: "4. console.log",
+        correct: "a4"
     }
 ];
 
 startQuizBtnEl.addEventListener("click", startQuiz);
+questionPageEl.addEventListener("click", questionAnswered);
